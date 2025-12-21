dcc5c7df396d93b233a0a1ae3f4fc932ecf0a968
ezhil <kezhil6@gmail.com>
2025-12-21 20:06:25 +0530

    Remainig work pushed

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagementApi.Repositories;
+﻿using LibraryManagementApi.Model;
+using LibraryManagementApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementApi.Controllers
@@ -7,12 +8,61 @@ namespace LibraryManagementApi.Controllers
     [Route("api/v1/[controller]")]
     public class BooksController : ControllerBase
     {
-        private readonly IBookService _bookServie;
+        private readonly IBookService _bookService;
         public BooksController(IBookService bookService)
         {
-            _bookServie = bookService;
+            _bookService = bookService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks(string? author, string? genre)
+        {
+            var books = await _bookService.GetAllBooksAsync(author, genre);
+            return Ok(books);
+        }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookById(int id)
+        {
+            var book = await _bookService.GetBookByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBook(Book book)
+        {
+            var createdBook = await _bookService.AddBookAsync(book);
+            return CreatedAtAction(nameof(GetBookById), new {id = createdBook.Id}, createdBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book book)
+        {
+            if(id != book.Id)
+            {
+                return BadRequest();
+            }   
+            var updatedBook = await _bookService.UpdateBookAsync(book);
+            if(updatedBook == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var deleted = await _bookService.DeleteBookAsync(id);
+            if(!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
